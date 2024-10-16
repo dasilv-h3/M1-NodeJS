2fcfb31a7de4c0f087200df099c53c30a6f06ccb
JovickT <jovicktchakala@yahoo.com>
2024-10-16 11:21:32 +0200

    [ADD] init crud news

---
+++ api/src/models/News.ts
@@ -0,0 +1,12 @@
+type News = {
+    id: number
+    title: string,
+    resume: string,
+    description: string,
+    image: string,
+    created_at: Date,
+    edit_at: Date,
+    club_id: number
+}
+
+export default News;
