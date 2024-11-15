1896d7ca15f152d90075e5fce76a895686c3a7b8
JovickT <jovicktchakala@yahoo.com>
2024-11-15 14:12:16 +0100

    [FIX] maj

--- front/react-app/src/routes/index.tsx
+++ front/react-app/src/routes/index.tsx
@@ -9,13 +9,24 @@ import Contact from "../pages/Contact";
 import Administration from "../pages/Administration";
 import Connexion from "../pages/Connexion";
 import Inscription from "../pages/Inscription";
-// import Partenaires from "../components/Sponsors";
+import AdminDashboard from "../pages/AdminDashboard/AdminDashBoard";
+import EditClub from "../pages/AdminDashboard/Club/EditClub";
+import ManageSponsors from "../pages/AdminDashboard/Sponsors/ManageSponsors";
+import AddSponsor from "../pages/AdminDashboard/Sponsors/AddSponsor";
+import EditSponsor from "../pages/AdminDashboard/Sponsors/EditSponsor";
+import ManageMatches from "../pages/AdminDashboard/Match/ManageMatches";
+import AddMatch from "../pages/AdminDashboard/Match/AddMatch";
+import EditMatch from "../pages/AdminDashboard/Match/EditMatch";
 
 const router = createBrowserRouter([
     {
 		path: "/",
 		element: <Home />,
-
+		// errorElement: <NotFoundPage />,
+	},
+    {
+		path: "/admin",
+		element: <AdminDashboard />,
 		// errorElement: <NotFoundPage />,
 	},
 	{
@@ -54,10 +65,51 @@ const router = createBrowserRouter([
 		path: "/inscription",
 		element: <Inscription />,
 	},
-	// {
-	// 	path: "/sponsors",
-	// 	element: <Partenaires />,
-	// },
+    {
+		path: "/admin/edit-club",
+		element: <EditClub />,
+		// errorElement: <NotFoundPage />,
+	},
+    {
+		path: "/admin/presentation",
+		// element: < />,
+		// errorElement: <NotFoundPage />,
+	},
+    {
+		path: "/admin/sponsors",
+		element: <ManageSponsors />,
+		// errorElement: <NotFoundPage />,
+	},
+    {
+		path: "/admin/sponsors/add-sponsor",
+		element: <AddSponsor />,
+		// errorElement: <NotFoundPage />,
+	},
+    {
+		path: "/admin/sponsors/edit-sponsor/:id",
+		element: <EditSponsor />,
+		// errorElement: <NotFoundPage />,
+	},
+    {
+		path: "/admin/manage-matches",
+		element: <ManageMatches />,
+		// errorElement: <NotFoundPage />,
+	},
+    {
+		path: "/admin/matches/add-match",
+		element: <AddMatch />,
+		// errorElement: <NotFoundPage />,
+	},
+    {
+		path: "/admin/matches/edit-match/:id",
+		element: <EditMatch />,
+		// errorElement: <NotFoundPage />,
+	},
+    {
+		path: "/admin/activate-account",
+		// element: < />,
+		// errorElement: <NotFoundPage />,
+	},
 ])
 
 export default router
