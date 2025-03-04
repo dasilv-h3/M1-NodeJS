28509751b6b53c87a42488c1965651118143f22f
JovickT <jovicktchakala@yahoo.com>
2025-03-04 10:45:17 +0100

    [ADD] navbar begin

--- mobile/lib/widgets/navbar.dart
+++ mobile/lib/widgets/navbar.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
 
-class Navbar extends StatefulWidget {
+class Navbar extends StatelessWidget {
   const Navbar({super.key});
 
   @override
-  State<Navbar> createState() => _NavbarState();
-}
-
-class _NavbarState extends State<Navbar> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Image.asset('assets/logo3.png', height: 40),
+        centerTitle: true,
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: const Icon(Icons.menu, color: Colors.blue),
+            onPressed: () {
+              Scaffold.of(context).openDrawer(); // Ouvre le menu
+            },
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue[900],
+              ),
+              child: const Text(
+                'Menu',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              title: const Text('Accueil'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Se connecter'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("S'inscrire"),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      body: const Center(
+        child: Text('Contenu principal ici'),
+      ),
+    );
   }
 }
